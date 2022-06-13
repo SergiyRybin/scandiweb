292a5e3a87e0eec60990bf18899dbce7c1374a57
SergiyRybin <serhiobax@gmail.com>
2022-06-14 00:06:35 +0300

    get apolo data

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+function ExchangeRates() {
+  const { data } = useQuery(gql`
+    query {
+      categories {
+        name
+      }
+    }
+  `);
+
+  if (data) {
+    console.log(data.categories);
+    return data.categories.map((el, index) => (
+      <div key={index}>
+        <p>{el.name}</p>
+      </div>
+    ));
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
